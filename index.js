03e301a26c163043b75a8e20edabbc8c433e28d3
akameco <akameco.t@gmail.com>
2017-04-06 16:08:31 +0900

    Add yarn && update deps

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 'use strict';
-var parseMs = require('parse-ms');
-var isFinite = require('is-finite');
+const parseMs = require('parse-ms');
+const isFinite = require('is-finite');
 
-module.exports = function (ms, opts) {
+module.exports = (ms, opts) => {
 	if (!isFinite(ms)) {
 		throw new TypeError('Expected a finite number');
 	}
@@ -12,9 +12,9 @@ module.exports = function (ms, opts) {
 	}
 
 	opts = opts || {};
-	var ret = [];
+	const ret = [];
 
-	var add = function (val, postfix) {
+	const add = function (val, postfix) {
 		if (val === 0) {
 			return;
 		}
@@ -22,15 +22,15 @@ module.exports = function (ms, opts) {
 		ret.push(val + postfix);
 	};
 
-	var parsed = parseMs(ms);
+	const parsed = parseMs(ms);
 
 	add(parsed.days, '日');
 	add(parsed.hours, '時間');
 	add(parsed.minutes, '分');
 
-	var sec = ms / 1000 % 60;
-	var secDecimalDigits = 1;
-	var secStr = sec.toFixed(secDecimalDigits).replace(/\.0$/, '');
+	const sec = ms / 1000 % 60;
+	const secDecimalDigits = 1;
+	const secStr = sec.toFixed(secDecimalDigits).replace(/\.0$/, '');
 	add(sec, '秒', secStr);
 
 	return ret.join(opts.split || '');
